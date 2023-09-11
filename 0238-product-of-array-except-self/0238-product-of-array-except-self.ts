7216cc28ada15399cc3f17f2a131355ddd9ac64c
Illya Plaksy <56319778+plaksy@users.noreply.github.com>
2023-09-11 17:50:04 +0300

    Time: 90 ms (70.61%), Space: 55.5 MB (35.62%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -0,0 +1,20 @@
+function productExceptSelf(nums: number[]): number[] {
+    
+    let len = nums.length;
+    let answer = new Array(len).fill(1);
+    let prefix = 1;
+    let postfix = 1;
+
+
+    for(let i = 0; i < len; i++) {
+        answer[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    for(let i = len - 1; i > -1; i--) {
+        answer[i] *= postfix;
+        postfix *= nums[i];
+    }
+    
+    return answer;
+};
