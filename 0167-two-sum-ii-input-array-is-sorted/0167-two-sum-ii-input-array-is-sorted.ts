78fecf84495b9a42e951d61aa0ab8e8c68adf196
Illya Plaksy <56319778+plaksy@users.noreply.github.com>
2023-09-13 15:46:36 +0300

    Time: 52 ms (93.13%), Space: 43.5 MB (58.06%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -1,16 +1,20 @@
 function twoSum(numbers: number[], target: number): number[] {
     
+
 let l = 0;
 let r = numbers.length-1;
 
 while (l < r) {
-    for(let i = r; i>l; i--) {
-        if(numbers[l] + numbers[i] == target) {
-            return [l+1, i+1];
-        }
+    let current = numbers[l] + numbers[r];
+    if(current > target) {
+        r -= 1;
     }
-    l += 1;
+    else if(current < target){
+        l += 1;
+    }
+    else {
+        return [l+1,r+1];
+    }
 }
-
 
 };
